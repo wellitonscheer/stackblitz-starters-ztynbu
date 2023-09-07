847c571b2a44368f9bd1eb64c9f743cef2b68978
wellitonscheer <78541441+wellitonscheer@users.noreply.github.com>
2023-09-06 21:42:44 -0300

    2 files modified

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export function Home() {
   return (
     <>
       <div>Home</div>
+      <Link to="/login">Login</Link>
     </>
   );
 }
--- src/account/login.tsx
+++ src/account/login.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export function Login() {
   return (
     <>
       <h1>Login</h1>
+      <Link to="/"> Home</Link>
     </>
   );
 }

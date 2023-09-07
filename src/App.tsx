548d41f8af107f3b5ddb7950fb112bdbc281017c
wellitonscheer <78541441+wellitonscheer@users.noreply.github.com>
2023-09-06 21:28:34 -0300

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Login } from './account/login';
 import './style.css';
 
-export const App: FC<{ name: string }> = ({ name }) => {
-  return (
-    <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+const router = createBrowserRouter([
+  { path: '/', element: <div>Home</div> },
+  { path: '/login', element: <Login /> },
+]);
+
+export const App = () => {
+  return <RouterProvider router={router} />;
 };

fe31745caa44522a924da78c071822ced0ccffd0
wellitonscheer <78541441+wellitonscheer@users.noreply.github.com>
2023-09-13 20:14:37 -0300

    1 file modified

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,25 +1,34 @@
-import { useEffect, useState } from "react"
-import { getDiscoverMovies } from "../services/moviedb";
+import { useEffect, useState } from 'react';
+import { getDiscoverMovies } from '../services/moviedb';
 
 export function UserPage() {
   const [movies, setMovies] = useState([]);
 
-  async function handleLoadMovies(){
-    const data = await getDiscoverMovies();
-    setMovies(data.results);
+  async function handleLoadMovies() {
+    try {
+      const data = await getDiscoverMovies();
+      setMovies(data.results);
+      console.log(data);
+    } catch {
+      console.log('deu ruim');
+    }
   }
 
   useEffect(() => {
+    console.log('tentou');
     handleLoadMovies();
-  }, [])
+  }, []);
 
   return (
     <>
       <h1>Bem-Vindo</h1>
-      <hr>
+      <hr />
       <h2>Seus Favoritos</h2>
-      <hr>
+      <hr />
       <h2>Veja outros lancamentos</h2>
+      {movies?.map((movie) => {
+        return <div>{movie.original_title}</div>;
+      })}
     </>
   );
 }
